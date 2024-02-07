dc1d00679f8fe0137f7a20d5929344001a41e8d7
dhanushramudri <ramudridhanush@gmail.com>
2024-02-08 00:35:50 +0530

    working correctly

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCOK5bcWIGsVhRSk_i8sVL_XyDTD-t9lVQ",
+  authDomain: "leetcode-4.firebaseapp.com",
+  projectId: "leetcode-4",
+  storageBucket: "leetcode-4.appspot.com",
+  messagingSenderId: "556317835766",
+  appId: "1:556317835766:web:c09c60c789d6f2974087d7",
+};
